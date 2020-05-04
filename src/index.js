c41693f8400b00583bf19da6ddd68d8aba71593a
glaveli <g.laveli.p@gmail.com>
2020-05-04 14:27:39 -0300

    process.env.PORT add

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
+
+let PORT = 3333;
+
 mongoose.connect('mongodb+srv://admin:Sem_senha1@s-i-m-w9uzj.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -15,4 +18,6 @@ app.use(express.json());
 app.use(routes);
 
 
-app.listen(3333);
+app.listen(process.env.PORT || PORT, () => {
+  console.log('Server is running on PORT:', PORT);
+});
